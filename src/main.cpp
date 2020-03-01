5fe7a960e718068eee517048f33733e49f010775
micro-pi <micro-pi.ru@yandex.ru>
2020-03-01 20:12:59 +0200

    Added module structure; Added Move Module (not finished)

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,14 @@
 
 #include <tasks.h>
 
+#include "modules/moving/MovingModule.h"
+
 int main() {
   BaseType_t xReturn;
+
+  MovingModule movingModule;
+  movingModule.init();
+  movingModule.mainFunction();
 
   xReturn = xTaskCreateAtProcessor(CORE_0, &vPowerBlinkTaskCore0, "vPowerBlinkTaskCore0", 256, NULL, 2, NULL);
   if (xReturn != pdPASS) {
